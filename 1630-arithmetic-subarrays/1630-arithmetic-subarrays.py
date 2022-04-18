e0cbca985ccaccd8a6fb177e94ca91deeec55b0f
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2022-04-18 20:07:28 +0530

    Time: 185 ms (97.96%), Space: 14.1 MB (93.37%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.py
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.py
@@ -1,12 +1,12 @@
 class Solution:
     def checkArithmeticSubarrays(self, nums: List[int], l: List[int], r: List[int]) -> List[bool]:
-        list=[]
+        list1=[]
         for i in range(len(l)):
             s=nums[l[i]:r[i]+1]
             s.sort()
-            d=Solution().helper(s)
-            list.append(d)
-        return list
+            x=Solution().helper(s)
+            list1.append(x)
+        return list1
     def helper(self,arr):
         if len(arr)==2:
             return True
