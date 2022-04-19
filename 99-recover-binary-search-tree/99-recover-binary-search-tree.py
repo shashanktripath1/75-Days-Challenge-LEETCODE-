746f90a4824a515ce55725133e5ec841c5191149
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2022-04-19 19:55:13 +0530

    Time: 100 ms (50.27%), Space: 14.4 MB (5.31%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.py
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.py
@@ -6,26 +6,26 @@
 #         self.right = right
 class Solution:
     def recoverTree(self, root: Optional[TreeNode]) -> None:
-        bucket=[]
+        list=[]
         def inorder(node):
-            if node!=None:
+            if node is not None:
                 inorder(node.left)
-                bucket.append(node)
+                list.append(node)
                 inorder(node.right)
         inorder(root)
-        
-        N=len(bucket)
-        firstnode=bucket[0]
-        lastnode=bucket[-1]
+        N=len(list)
+        first_node=list[0]
+        last_node=list[-1]
         
         for i in range(1,N):
-            if bucket[i].val < bucket[i-1].val:
-                firstnode=bucket[i-1]
+            if list[i].val<list[i-1].val:
+                first_node=list[i-1]
                 break
         for i in range(N-2,-1,-1):
-            if bucket[i].val>bucket[i+1].val:
-                lastnode=bucket[i+1]
+            if list[i].val>list[i+1].val:
+                last_node=list[i+1]
                 break
-        firstnode.val,lastnode.val=lastnode.val,firstnode.val
-        
+        first_node.val,last_node.val=last_node.val,first_node.val
+                
+                
         
