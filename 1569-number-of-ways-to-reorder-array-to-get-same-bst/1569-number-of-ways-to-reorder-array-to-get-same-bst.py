dd1fb50309246a0538b79a373eb94e6f84fd47bc
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2022-06-27 21:33:37 +0530

    Time: 451 ms (12.50%), Space: 19.1 MB (81.88%) - LeetHub

--- 1569-number-of-ways-to-reorder-array-to-get-same-bst/1569-number-of-ways-to-reorder-array-to-get-same-bst.py
+++ 1569-number-of-ways-to-reorder-array-to-get-same-bst/1569-number-of-ways-to-reorder-array-to-get-same-bst.py
@@ -3,11 +3,11 @@ class Solution:
     def numOfWays(self, nums: List[int]) -> int:
         def ncr(n,r):
             f=math.factorial
-            return f(n) // (f(r)*f(n-r))
+            return f(n)//(f(r)*f(n-r))
         def method1(nums):
+            left,right=[],[]
             if len(nums)<=2:
                 return 1
-            left,right=[],[]
             for i in range(1,len(nums)):
                 if nums[i]>nums[0]:
                     right.append(nums[i])
@@ -15,4 +15,3 @@ class Solution:
                     left.append(nums[i])
             return ncr(len(left)+len(right),len(right))*method1(left)*method1(right)
         return (method1(nums)-1)%(10**9+7)
-            
