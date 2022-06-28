5160628e66cb985c3898874d7097674d00d4e2fe
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2022-06-28 19:35:46 +0530

    Time: 509 ms (11.11%), Space: 15.5 MB (5.13%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -1,6 +1,12 @@
 class Solution:
     def minDeletions(self, s: str) -> int:
-        hash_map=collections.Counter(s)
+        hash_map={}
+        s=list(s)
+        for i in range(len(s)):
+            if s[i] in hash_map:
+                hash_map[s[i]]+=1
+            else:
+                hash_map[s[i]]=1
         deletions=0
         freq_set=set()
         for key,value in hash_map.items():
