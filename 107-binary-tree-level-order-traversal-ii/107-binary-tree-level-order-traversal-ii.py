853b228c9414b31c87bf4d5d7183cffacc7878af
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2022-06-30 22:23:04 +0530

    Time: 68 ms (16.29%), Space: 14.2 MB (82.68%) - LeetHub

--- 107-binary-tree-level-order-traversal-ii/107-binary-tree-level-order-traversal-ii.py
+++ 107-binary-tree-level-order-traversal-ii/107-binary-tree-level-order-traversal-ii.py
@@ -6,20 +6,20 @@
 #         self.right = right
 class Solution:
     def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
-        res=[]
+        bfs=[]
         if not root:
             return []
-        queue=deque()
+        queue=deque([])
         queue.append(root)
         while queue:
+            level_size=len(queue)
             cur_level=[]
-
-            for i in range(len(queue)):
+            for i in range(level_size):
                 cur=queue.popleft()
                 cur_level.append(cur.val)
-                if cur.left is not None:
+                if cur.left:
                     queue.append(cur.left)
-                if cur.right is not None:
+                if cur.right:
                     queue.append(cur.right)
-            res.append(cur_level)
-        return res[::-1]
+            bfs.append(cur_level)
+        return bfs[::-1]
