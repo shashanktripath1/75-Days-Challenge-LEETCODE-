5119ec5c726c2df2c01d41c91a0f1679f73997b2
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2022-07-19 19:45:31 +0530

    Time: 43 ms (63.47%), Space: 13.9 MB (66.48%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -1,11 +1,8 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        res=[[1]]
-        for i in range(numRows-1):
-            temp=[0]+res[-1]+[0]
-            row=[]
-            for j in range(len(res[-1])+1):
-                row.append(temp[j]+temp[j+1])
-            res.append(row)
-        return res
-        
+        matrix=[[1 for i in range(row+1)]for row in range(numRows)]
+        for i in range(numRows):
+            for j in range(1,i):
+                matrix[i][j]=matrix[i-1][j]+matrix[i-1][j-1]
+        return matrix
+                
