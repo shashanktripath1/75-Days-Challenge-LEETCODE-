9b72fe72101356357491537054fa5fbd722cf346
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2022-07-24 12:35:12 +0530

    Time: 315 ms (25.38%), Space: 20.3 MB (84.45%) - LeetHub

---
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def searchMatrix(self, matrix, target):
+        j = -1
+        for row in matrix:
+            while j + len(row) and row[j] > target:
+                j -= 1
+            if row[j] == target:
+                return True
+        return False
