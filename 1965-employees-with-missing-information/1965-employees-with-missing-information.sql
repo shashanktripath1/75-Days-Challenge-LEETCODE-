b961a905a468010c298f17b267ea9dff330dce0d
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2022-07-28 11:30:11 +0530

    Time: 829 ms (27.07%), Space: 0B (100.00%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -1,8 +1,8 @@
-SELECT T.employee_id 
-FROM 
-    (SELECT * FROM Employees LEFT JOIN Salaries using(employee_id)
-    UNION
-    SELECT * FROM Employees RIGHT JOIN Salaries using(employee_id))
-AS T
-WHERE T.salary IS NULL OR T.name IS NULL
-ORDER BY employee_id
+SELECT S.employee_id
+FROM (SELECT * FROM Employees LEFT JOIN Salaries USING(employee_id)
+     UNION
+      SELECT * FROM Employees RIGHT JOIN Salaries USING(employee_id)
+     )
+AS S
+WHERE S.salary is NULL or S.name is NULL
+OrDER BY employee_id
