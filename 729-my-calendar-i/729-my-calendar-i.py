115f8951f882b9d305fea6ab35f59c281689cf6a
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2022-08-03 11:19:43 +0530

    Time: 801 ms (26.94%), Space: 14.7 MB (58.88%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.py
+++ 729-my-calendar-i/729-my-calendar-i.py
@@ -1,19 +1,10 @@
 class MyCalendar:
-
     def __init__(self):
-        self.meet = []
+        self.eventDates = []
 
     def book(self, start: int, end: int) -> bool:
-        if not self.meet:
-            self.meet.append([start, end])
-            return True
-        
-        temp = self.meet.copy()
-        temp.append([start, end])
-        temp.sort(key = lambda i: i[0])
-        for i in range(1, len(temp)):
-            if temp[i][0] < temp[i-1][1]:
-                return False
-        
-        self.meet = temp.copy()
+        for laststart,lastend in self.eventDates:
+            if laststart < end and start < lastend: 
+                return False 
+        self.eventDates.append((start,end))
         return True
