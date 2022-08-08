e572b4ae3a477c128e69478bd0aeef988f4103aa
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2022-08-08 14:17:40 +0530

    Time: 873 ms (67.60%), Space: 30.3 MB (43.41%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.py
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -1,15 +1,14 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        ans=[]
+        res=[]
         l=len(s)
-        def dfs(i,temp):
+        def dfs(i,ds):
             if i==l:
-                ans.append(temp)
+                res.append(ds)
             j=i+1
-            while(j<l+1):
+            while (j<l+1):
                 if s[i:j]==s[i:j][::-1]:
-                    dfs(j,temp+[s[i:j]])
+                    dfs(j,ds+[s[i:j]])
                 j+=1
         dfs(0,[])
-        return ans
-                
+        return res
