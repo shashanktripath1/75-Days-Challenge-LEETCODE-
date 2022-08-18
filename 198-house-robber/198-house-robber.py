8a29e73a447159ed8eb80440d3f5c2fbd69bc62b
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2022-08-18 21:20:04 +0530

    Time: 46 ms (60.99%), Space: 13.9 MB (66.15%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,17 +1,13 @@
+#tabulation
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        def f(ind,dp):
-            
-            if ind==0:
-                return nums[ind]
-            if ind<0:
-                return 0
-            if dp[ind]!=-1:
-                return dp[ind]
-            take=nums[ind]+f(ind-2,dp)
-            not_take=0+f(ind-1,dp)
-            dp[ind]= max(take,not_take)
-            return dp[ind]
         n=len(nums)
-        dp=[-1]*n
-        return f(n-1,dp)
+        dp=[0]*n
+        dp[0]=nums[0]
+        for i in range(1,n):
+            take=nums[i]
+            if i>1:
+                take+=dp[i-2]
+            not_take=0+dp[i-1]
+            dp[i]=max(take,not_take)
+        return dp[-1]
