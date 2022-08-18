7c1f05dd0417a2b9405cd499cdfa951f2fc6140c
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2022-08-18 10:39:19 +0530

    Time: 587 ms (98.35%), Space: 31.4 MB (94.54%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
@@ -1,16 +1,11 @@
 class Solution:
     def minSetSize(self, arr: List[int]) -> int:
-        d={}
-        for i in arr:
-            if i in d:
-                d[i]+=1
-            else:
-                d[i]=1
-        lst=list(d.values())
-        lst.sort(reverse=True)
-        ans,count,index,size=0,0,0,len(arr)
-        while(count*2<size):
+        d=collections.Counter(arr)
+        l=list(d.values())
+        l.sort(reverse=True)
+        ans,count,index=0,0,0
+        while(count*2<len(arr)):
             ans+=1
-            count+=lst[index]
+            count+=l[index]
             index+=1
         return ans
