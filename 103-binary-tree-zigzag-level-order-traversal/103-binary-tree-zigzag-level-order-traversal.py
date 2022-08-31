ef4ca1d175179e1b5d03c4de04df398c4fac7522
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2022-08-31 14:21:03 +0530

    Time: 31 ms (96.53%), Space: 14.1 MB (58.05%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.py
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.py
@@ -1,25 +1,23 @@
-# Definition for a binary tree node.
 from collections import *
-
-class TreeNode:
-        def __init__(self, val=0, left=None, right=None):
-            self.val = val
-            self.left = left
-            self.right = right
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        bfs=[]
-        left_to_right=True
-        if root is None:
-            return bfs
-        queue=deque([])
+        ans=[]
+        flag=True
+        queue=deque()
         queue.append(root)
+        if not root:
+            return ans
         while queue:
-            level_size=len(queue)
             cur_level=deque()
-            for i in range(level_size):
+            for i in range(len(queue)):
                 cur=queue.popleft()
-                if left_to_right:
+                if flag:
                     cur_level.append(cur.val)
                 else:
                     cur_level.appendleft(cur.val)
@@ -27,6 +25,9 @@ class Solution:
                     queue.append(cur.left)
                 if cur.right is not None:
                     queue.append(cur.right)
-            bfs.append(cur_level)
-            left_to_right=not left_to_right
-        return bfs
+            ans.append(cur_level)
+            flag=not flag
+        return ans
+        
+
+                
