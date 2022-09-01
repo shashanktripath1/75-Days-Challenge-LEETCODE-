b6d49b27738fa9c89f1318c813e1928857f4cddf
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2022-09-01 14:39:01 +0530

    Time: 5248 ms (18.58%), Space: 21.9 MB (74.67%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
@@ -1,30 +1,29 @@
 class Solution:
     def cherryPickup(self, grid: List[List[int]]) -> int:
-        def f(i,j1,j2,dp):
-            if j1<0 or j1>=m or j2<0 or j2>=m:
-                return -1e9
-            if i==n-1:
-                if j1==j2:
-                    return grid[i][j1]
-                else:
-                    return grid[i][j1]+grid[i][j2]
-            if dp[i][j1][j2]!=-1:
-                return dp[i][j1][j2]
-            maxi=float('-inf')
-            for dj1 in range(-1,2):
-                for dj2 in range(-1,2):
-                    ans=0
-                    if j1==j2:
-                        ans=grid[i][j1]
-                    else:
-                        ans=grid[i][j1]+grid[i][j2]
-                    ans+=f(i+1,j1+dj1,j2+dj2,dp)
-                    maxi=max(maxi,ans)
-            dp[i][j1][j2]=maxi
-            return dp[i][j1][j2]
-        
-
         n,m=len(grid),len(grid[0])
-        dp = [[[-1 for i in range(m)] for j1 in range(n)] for j2 in range(n)]
-
-        return f(0,0,m-1,dp)
+        dp=[[[-1]*m for j in range(m)]for k in range(n)]
+        for j1 in range(m):
+            for j2 in range(m):
+                if j1==j2:
+                    dp[n-1][j1][j2]=grid[n-1][j1]
+                else:
+                    dp[n-1][j1][j2]=grid[n-1][j1]+grid[n-1][j2]
+        for i in range(n-2,-1,-1):
+            for j1 in range(m):
+                for j2 in range(m):
+                    maxi=0
+                    for di in range(-1,2):
+                        for dj in range(-1,2):
+                            ans=0
+                            if j1 + di < 0 or j1 + di >= m or j2 + dj < 0 or j2 + dj >= m:
+                                continue
+                            if j1==j2:
+                                ans=grid[i][j1]
+                            else:
+                                ans=grid[i][j1]+grid[i][j2]
+                            ans += dp[i + 1][j1 + di][j2 + dj]
+                            maxi=max(maxi,ans)
+           
+                    dp[i][j1][j2]=maxi
+        return dp[0][0][m-1]
+                
