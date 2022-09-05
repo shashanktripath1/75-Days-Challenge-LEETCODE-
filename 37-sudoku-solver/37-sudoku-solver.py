751304ee75348a4a05b09813b467203f49e97521
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2022-09-05 15:51:06 +0530

    Time: 919 ms (25.57%), Space: 13.9 MB (96.01%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.py
+++ 37-sudoku-solver/37-sudoku-solver.py
@@ -1,29 +1,29 @@
 class Solution:
     def solveSudoku(self, board: List[List[str]]) -> None:
-        def helper_valid(board,r,c,n_str):
+        def helper(board,r,c,s):
             for j in range(9):
-                if board[r][j]==n_str:
+                if board[r][j]==s:
                     return False
             for i in range(9):
-                if board[i][c]==n_str:
+                if board[i][c]==s:
                     return False
-            x=r//3
-            y=c//3
+            x,y=r//3,c//3
             for i in range(3*x,3*x+3):
                 for j in range(3*y,3*y+3):
-                    if board[i][j]==n_str:
+                    if board[i][j]==s:
                         return False
             return True
+                    
+            
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if board[i][j]==".":
-                    for n_str in "123456789":
-                        if helper_valid(board,i,j,n_str):
-                            board[i][j]=n_str
+                if board[i][j]=='.':
+                    for s in "123456789":
+                        if helper(board,i,j,s):
+                            board[i][j]=s
                             if self.solveSudoku(board):
                                 return True
                             else:
-                                board[i][j]="."
+                                board[i][j]='.'
                     return False
         return True
-    
