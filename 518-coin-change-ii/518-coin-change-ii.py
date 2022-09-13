050106110f5bf6a9bba02c03733baaef2a44dba6
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2022-09-13 22:21:23 +0530

    Time: 1012 ms (27.32%), Space: 35.2 MB (27.00%) - LeetHub

--- 518-coin-change-ii/518-coin-change-ii.py
+++ 518-coin-change-ii/518-coin-change-ii.py
@@ -1,18 +1,21 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        def helper(ind,tar,dp):
-            if ind==0:
-                if (tar%coins[0]==0):
-                    return 1
-                return 0
-            if dp[ind][tar]!=-1:
-                return dp[ind][tar]
-            not_take=helper(ind-1,tar,dp)
-            take=0
-            if coins[ind]<=tar:
-                take=helper(ind,tar-coins[ind],dp)
-            dp[ind][tar]=take+not_take
-            return dp[ind][tar]
+        
         n=len(coins)
-        dp=[[-1 for i in range(amount+1)]for j in range(n+1)]
-        return helper(n-1,amount,dp)
+        dp=[[0 for i in range(amount+1)]for j in range(n+1)]
+
+        for t in range(amount+1):
+            if(t%coins[0]==0):
+                dp[0][t]+=1
+            dp[0][t]+=0
+        for ind in range(1,n):
+            for tar in range(amount+1):
+                not_take=dp[ind-1][tar]
+                take=0
+                if coins[ind]<=tar:
+                    take=dp[ind][tar-coins[ind]]
+                dp[ind][tar]=take + not_take
+        return dp[n-1][amount]
+                
+        
+           
