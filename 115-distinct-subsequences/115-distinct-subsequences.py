fbb9486e7d29f98e0ee5fd99009a4be8076f5c6f
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2022-09-16 13:36:34 +0530

    Time: 1788 ms (13.27%), Space: 71.4 MB (43.82%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.py
+++ 115-distinct-subsequences/115-distinct-subsequences.py
@@ -1,19 +1,17 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
-        def f(i,j,s,t,dp):
-            if j==0:
-                return 1
-            if i==0:
-                return 0
-            if dp[i][j]!=-1:
-                return dp[i][j]
-            if s[i-1]==t[j-1]:
-                dp[i][j]= f(i-1,j-1,s,t,dp)+f(i-1,j,s,t,dp)
-                return dp[i][j]
-            
-            dp[i][j]=f(i-1,j,s,t,dp)
-            return dp[i][j]
         n=len(s)
         m=len(t)
-        dp=[[-1 for i in range(m+1)]for j in range(n+1)]
-        return f(n,m,s,t,dp)
+        dp=[[0 for i in range(m+1)]for j in range(n+1)]
+        for i in range(n+1):
+            dp[i][0]=1
+        for j in range(1,m+1):
+            dp[0][j]=0
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if s[i-1]==t[j-1]:
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j]
+                else:
+                    dp[i][j]=dp[i-1][j]
+        return dp[n][m]
+                
