8c4ddeff8b74bcb12e015ef2b3aa6b8379e7613c
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2022-09-18 22:57:21 +0530

    Time: 413 ms (14.05%), Space: 17.1 MB (72.36%) - LeetHub

--- 72-edit-distance/72-edit-distance.py
+++ 72-edit-distance/72-edit-distance.py
@@ -1,13 +1,14 @@
+#one based indexing for tabulation to avoid minus indexes
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         def f(i,j,dp):
-            if i<0:
-                return j+1
-            if j<0:
-                return i+1
+            if i==0:
+                return j
+            if j==0:
+                return i
             if dp[i][j]!=-1:
                 return dp[i][j]
-            if word1[i]==word2[j]:
+            if word1[i-1]==word2[j-1]:
                 return 0+f(i-1,j-1,dp)
             insert=1+f(i,j-1,dp)
             delete=1+f(i-1,j,dp)
@@ -17,4 +18,4 @@ class Solution:
         n=len(word1)
         m=len(word2)
         dp=[[-1 for _ in range(m+1)]for _ in range(n+1)]
-        return f(n-1,m-1,dp)
+        return f(n,m,dp)
