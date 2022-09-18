92ebf8a737c66cbc1dbd4624dcd85051befb4470
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2022-09-18 23:06:48 +0530

    Time: 468 ms (9.60%), Space: 17.9 MB (15.76%) - LeetHub

--- 72-edit-distance/72-edit-distance.py
+++ 72-edit-distance/72-edit-distance.py
@@ -17,5 +17,18 @@ class Solution:
             return dp[i][j]
         n=len(word1)
         m=len(word2)
-        dp=[[-1 for _ in range(m+1)]for _ in range(n+1)]
-        return f(n,m,dp)
+        dp=[[0 for _ in range(m+1)]for _ in range(n+1)]
+        for j in range(m+1):
+            dp[0][j]=j
+        for i in range(n+1):
+            dp[i][0]=i
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if word1[i-1]==word2[j-1]:
+                    dp[i][j]=dp[i-1][j-1]
+                else:
+                    insert=1+dp[i][j-1]
+                    delete=1+dp[i-1][j]
+                    replace=1+dp[i-1][j-1]
+                    dp[i][j]= min(insert,min(delete,replace))
+        return dp[n][m]
