4acb9637a20da98601ceb4cd00da6b0d03cf5ef9
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2022-09-19 21:49:26 +0530

    Time: 2107 ms (9.55%), Space: 23 MB (46.09%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.py
+++ 44-wildcard-matching/44-wildcard-matching.py
@@ -1,28 +1,25 @@
-#one based indexing for tabulation
+#tabulation
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        def f(i,j,dp):
-            if i==0 and j==0:
-                return True
-            if j==0 and i>0:
-                return False
-            if i==0 and j>0:
-                for ii in range(1,j+1):
-                    if p[ii-1]!='*':
-                        return False
-                return True
-            if dp[i][j]!=-1:
-                return dp[i][j]
-            if s[i-1]==p[j-1] or p[j-1]=="?":
-                dp[i][j]= f(i-1,j-1,dp)
-                return dp[i][j]
-            if p[j-1]=="*":
-                dp[i][j]=f(i-1,j,dp) or f(i,j-1,dp)
-                return dp[i][j]
-            dp[i][j]=False
-            return dp[i][j]
-
         n=len(s)
         m=len(p)
-        dp=[[-1 for i in range(m+1)]for j in range(n+1)]
-        return f(n,m,dp)
+        dp=[[False for i in range(m+1)]for j in range(n+1)]
+        dp[0][0]=True
+        for i in range(1,n+1):
+            dp[i][0]=False
+        for j in range(1,m+1):
+            flag=True
+            for ii in range(1,j+1):
+                if p[ii-1]!='*':
+                    flag=False
+                    break
+            dp[0][j]=flag
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if s[i-1]==p[j-1] or p[j-1]=="?":
+                    dp[i][j]=dp[i-1][j-1]
+                elif p[j-1]=="*":
+                    dp[i][j]=dp[i-1][j] or dp[i][j-1]
+                else:
+                    dp[i][j]=False
+        return dp[n][m]
