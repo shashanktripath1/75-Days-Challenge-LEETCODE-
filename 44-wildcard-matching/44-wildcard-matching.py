e053c74d3215cd44f8ff32a32df020305b449a5d
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2022-09-19 21:23:37 +0530

    Time: 555 ms (81.52%), Space: 24.1 MB (38.28%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.py
+++ 44-wildcard-matching/44-wildcard-matching.py
@@ -1,7 +1,6 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        @cache
-        def f(i,j):
+        def f(i,j,dp):
             if i<0 and j<0:
                 return True
             if j<0 and i>=0:
@@ -11,12 +10,18 @@ class Solution:
                     if p[ii]!='*':
                         return False
                 return True
+            if dp[i][j]!=-1:
+                return dp[i][j]
             if s[i]==p[j] or p[j]=="?":
-                return f(i-1,j-1)
+                dp[i][j]= f(i-1,j-1,dp)
+                return dp[i][j]
             if p[j]=="*":
-                return f(i-1,j) or f(i,j-1)
-            return False
+                dp[i][j]=f(i-1,j,dp) or f(i,j-1,dp)
+                return dp[i][j]
+            dp[i][j]=False
+            return dp[i][j]
 
         n=len(s)
         m=len(p)
-        return f(n-1,m-1)
+        dp=[[-1 for i in range(m+1)]for j in range(n+1)]
+        return f(n-1,m-1,dp)
