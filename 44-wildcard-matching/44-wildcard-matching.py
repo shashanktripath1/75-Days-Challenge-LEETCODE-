96ad4f99088be352174ca7a98bc82b92ce0d6a42
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2022-09-19 21:29:01 +0530

    Time: 1355 ms (34.74%), Space: 23.9 MB (40.42%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.py
+++ 44-wildcard-matching/44-wildcard-matching.py
@@ -1,21 +1,22 @@
+#one based indexing for tabulation
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         def f(i,j,dp):
-            if i<0 and j<0:
+            if i==0 and j==0:
                 return True
-            if j<0 and i>=0:
+            if j==0 and i>0:
                 return False
-            if i<0 and j>=0:
-                for ii in range(j+1):
-                    if p[ii]!='*':
+            if i==0 and j>0:
+                for ii in range(1,j+1):
+                    if p[ii-1]!='*':
                         return False
                 return True
             if dp[i][j]!=-1:
                 return dp[i][j]
-            if s[i]==p[j] or p[j]=="?":
+            if s[i-1]==p[j-1] or p[j-1]=="?":
                 dp[i][j]= f(i-1,j-1,dp)
                 return dp[i][j]
-            if p[j]=="*":
+            if p[j-1]=="*":
                 dp[i][j]=f(i-1,j,dp) or f(i,j-1,dp)
                 return dp[i][j]
             dp[i][j]=False
@@ -24,4 +25,4 @@ class Solution:
         n=len(s)
         m=len(p)
         dp=[[-1 for i in range(m+1)]for j in range(n+1)]
-        return f(n-1,m-1,dp)
+        return f(n,m,dp)
