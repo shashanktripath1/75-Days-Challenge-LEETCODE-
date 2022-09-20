4f974e95d84dd25aea0a126a6206edc98ef835be
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2022-09-20 21:56:28 +0530

    Time: 306 ms (34.79%), Space: 18.9 MB (34.08%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,14 +1,12 @@
 class Solution:
-    def maxProfit(self, k, prices):
-        n = len(prices)
-        if n <= 1 or k== 0: return 0
-        
-        dp = [[0]*n for i in range(k+1)]
-        
-        for t in range(1, k+1):    
-            prevMax = float("-inf") 
-            for d in range(1, n):  
-                prevMax = max(prevMax, dp[t-1][d-1] - prices[d-1])
-                dp[t][d] = max(prevMax + prices[d], dp[t][d-1])
-                
-        return dp[-1][-1]
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        n=len(prices)
+        dp=[[[0]*(k+1) for _ in range(2)]for _ in range(n+1)]
+        for ind in range(n-1,-1,-1):
+            for buy in range(2):
+                for cap in range(1,k+1):
+                    if(buy==1):
+                        dp[ind][buy][cap]=max(-prices[ind]+dp[ind+1][0][cap],dp[ind+1][1][cap])
+                    else:
+                        dp[ind][buy][cap]=max(prices[ind]+dp[ind+1][1][cap-1],dp[ind+1][0][cap]) 
+        return dp[0][1][k]
