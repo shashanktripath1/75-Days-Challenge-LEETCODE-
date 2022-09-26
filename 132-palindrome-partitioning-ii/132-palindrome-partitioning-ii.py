55c24ae76c68c9367111ebddd4d74b70dc90eda3
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2022-09-26 18:50:54 +0530

    Time: 2991 ms (43.95%), Space: 14.2 MB (73.61%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.py
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.py
@@ -18,5 +18,15 @@ class Solution:
             return dp[i]
                 
         n=len(s)
-        dp=[-1]*(n+1)
-        return f(0,n,s,dp)-1
+        dp=[0]*(n+1)
+        dp[n]=0
+        for i in range(n-1,-1,-1):
+            temp=""
+            min_cost=float('inf')
+            for j in range(i,n):
+                temp+=s[j]
+                if (ispalindrome(temp)):
+                    cost=1+dp[j+1]
+                    min_cost=min(min_cost,cost)
+            dp[i]= min_cost
+        return dp[0]-1
