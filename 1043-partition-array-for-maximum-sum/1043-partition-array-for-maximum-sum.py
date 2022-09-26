a040a9c58a4cfedeedd96e35aa4844f12fb6d771
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2022-09-26 22:04:11 +0530

    Time: 1028 ms (57.94%), Space: 14.4 MB (37.40%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
@@ -18,5 +18,15 @@ class Solution:
             
 
         n=len(arr)
-        dp=[-1]*(n+1)
-        return f(0,k,dp)
+        dp=[0]*(n+1)
+        for ind in range(n-1,-1,-1):
+            len1=0
+            max_ans=float('-inf')
+            maxi=float('-inf')
+            for j in range(ind,min(n,ind+k)):
+                len1+=1
+                maxi=max(maxi,arr[j])
+                sum1=(len1 * maxi)+dp[j+1]
+                max_ans=max(max_ans,sum1)
+            dp[ind]=max_ans
+        return dp[ind]
