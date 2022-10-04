dec188dc134bb7d81c6f88c6bcd9168308801771
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2022-10-04 10:15:23 +0530

    Time: 1481 ms (5.01%), Space: 22 MB (15.97%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,22 +1,26 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def dfs(i,j):
-            if i<0 or i>=m or j<0 or j>=n or (i,j) in visited or grid[i][j]=='0':
-                return
+        def bfs(i,j):
+            queue=deque()
+            queue.append((i,j))
             visited.add((i,j))
-            dfs(i+1,j)
-            dfs(i-1,j)
-            dfs(i,j+1)
-            dfs(i,j-1)
+            while queue:
+                r,c=queue.popleft()
+                directions=[(0,1),(0,-1),(1,0),(-1,0)]
+                for dr,dc in directions:
+                    row,col=r+dr,c+dc
+                    if row in range(m) and col in range(n) and grid[row][col]=='1' and (row,col) not in visited:
+                        queue.append((row,col))
+                        visited.add((row,col))
+
         m,n=len(grid),len(grid[0])
         visited=set()
         ans=0
         for i in range(m):
             for j in range(n):
-                if (i,j) not in visited and grid[i][j]=='1':
+                if grid[i][j]=='1' and (i,j) not in visited:
                     ans+=1
-                    dfs(i,j)
+                    bfs(i,j)
         return ans
 
 
-        
