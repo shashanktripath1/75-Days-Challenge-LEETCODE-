bc8fa58f0041667a98d6e85ac10f32f15adb0a62
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2022-10-09 17:48:16 +0530

    Time: 359 ms (25.11%), Space: 15.8 MB (73.06%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.py
+++ 130-surrounded-regions/130-surrounded-regions.py
@@ -3,7 +3,7 @@ class Solution:
         def dfs(i,j):
             if i<0 or i>=m or j<0 or j>=n or board[i][j]!='O':
                 return
-            board[i][j]='#'
+            board[i][j]='#'  #CONVERTING TO A DOLLAR SIGN
             dfs(i+1,j)
             dfs(i-1,j)
             dfs(i,j-1)
@@ -15,18 +15,18 @@ class Solution:
         for i in range(m):
             for j in range(n):
                 if i==0 or i==m-1:
-                    dfs(i,j)
+                    dfs(i,j)#CALLING DFS ON ALL BORDER 0'S
                 if j==0 or j==n-1:
                     dfs(i,j)
         
         for i in range(m):
             for j in range(n):
                 if board[i][j]=='O':
-                    board[i][j]='X'
+                    board[i][j]='X'#LEFT OUT ZEROES ARE TURNED TO X
         
         for i in range(m):
             for j in range(n):
                 if board[i][j]=='#':
-                    board[i][j]='O'
+                    board[i][j]='O'#CONVERTING BORDER ZEROES INTO ORIGINAL FORMS FROM # TO 'o'
         return board
         
