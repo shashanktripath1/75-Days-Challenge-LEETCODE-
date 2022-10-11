4cd8200c112111fc3906cca9b253fa74eb788454
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2022-10-11 21:21:03 +0530

    Time: 733 ms (86.14%), Space: 75.9 MB (33.14%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -1,20 +1,22 @@
 class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
-        def bfs(i,j):
+        #this bfs function will make all the boundary 1's and the 1's connected to them as 0
+        def dfs(i,j):
             if i<0 or i>=m or j<0 or j>=n or grid[i][j]==0:
                 return 0
             grid[i][j]=0
-            bfs(i+1,j)
-            bfs(i-1,j)
-            bfs(i,j-1)
-            bfs(i,j+1)
+            dfs(i+1,j)
+            dfs(i-1,j)
+            dfs(i,j-1)
+            dfs(i,j+1)
         
         m,n=len(grid),len(grid[0])
         for i in range(m):
             for j in range(n):
                 if (i==0 or i==m-1 or j==0 or j==n-1) and grid[i][j]==1:
-                    bfs(i,j)
-        count=0            
+                    dfs(i,j)
+        count=0
+        #now counting the remaining 1's and that will be our answer
         for i in range(m):
             for j in range(n):
                 if grid[i][j]==1:
