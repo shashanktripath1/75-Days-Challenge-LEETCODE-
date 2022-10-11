e0f96adc0170fc6725bd48d40b33ab5bf95c8c2a
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2022-10-11 20:17:52 +0530

    Time: 936 ms (71.83%), Space: 76 MB (33.14%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -4,7 +4,10 @@ class Solution:
             if i<0 or i>=m or j<0 or j>=n or grid[i][j]==0:
                 return 0
             grid[i][j]=0
-            return 1+bfs(i+1,j)+bfs(i-1,j)+bfs(i,j-1)+bfs(i,j+1)
+            bfs(i+1,j)
+            bfs(i-1,j)
+            bfs(i,j-1)
+            bfs(i,j+1)
         
         m,n=len(grid),len(grid[0])
         for i in range(m):
