7d6f06fa53ebbff6a326470a7bab490af3d93386
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2022-11-12 18:13:07 +0530

    Time: 756 ms (79.12%), Space: 36.8 MB (5.14%) - LeetHub

---
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -0,0 +1,15 @@
+from sortedcontainers import SortedList
+class MedianFinder:
+
+    def __init__(self):
+        self.arr=SortedList([])
+
+    def addNum(self, num: int) -> None:
+        self.arr.add(num)
+        
+    def findMedian(self) -> float:
+        n=len(self.arr)
+        if(n%2==0):
+            return(self.arr[n//2]+self.arr[n//2-1])/2
+        else:
+            return(self.arr[n//2])
