d765375b844c093a2705a158e83e9c0a76a4cc10
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-01-20 16:29:14 +0530

    Time: 31 ms (94.73%), Space: 16.6 MB (55.22%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,3 +1,18 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        return nums.sort()
+        count_0,count_1,count_2=0,0,0
+        for i in nums:
+            if i==0:
+                count_0+=1
+            elif i==1:
+                count_1+=1
+            else:
+                count_2+=1
+        for i in range(count_0):
+            nums[i]=0
+        for i in range(count_0,count_0+count_1):
+            nums[i]=1
+        for i in range(count_0+count_1,len(nums)):
+            nums[i]=2
+        return nums
+        
