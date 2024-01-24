a6c48043786df34dcd75a4c6ddf724b65c5889d9
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-01-24 22:54:02 +0530

    Time: 93 ms (97.82%), Space: 17.5 MB (61.00%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,22 +1,16 @@
 class Solution:
-    def mark_row(self,matrix,m,n,i):
-        for j in range(n):
-            if matrix[i][j]!=0:
-                matrix[i][j]=-12345
-    def mark_col(self,matrix,m,n,j):
-        for i in range(m):
-            if matrix[i][j]!=0:
-                matrix[i][j]=-12345
     def setZeroes(self, matrix: List[List[int]]) -> None:
         m,n=len(matrix),len(matrix[0])
+        row=[0]*(m)
+        col=[0]*(n)
         for i in range(m):
             for j in range(n):
                 if matrix[i][j]==0:
-                    self.mark_row(matrix,m,n,i)
-                    self.mark_col(matrix,m,n,j)
+                    row[i]=1
+                    col[j]=1
         for i in range(m):
             for j in range(n):
-                if matrix[i][j]== -12345:
+                if row[i]==1 or col[j]==1:
                     matrix[i][j]=0
         return matrix
-                    
+        
