99eb821b7de1d06c60575c4c19b9add0828e0241
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-01-31 22:40:40 +0530

    Time: 78 ms (99.98%), Space: 17.9 MB (70.02%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,13 +1,32 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        d={}
-        ans=[]
-        for i in nums:
-            if i not in d:
-                d[i]=1
+        n=len(nums)
+        cnt1,cnt2=0,0
+        ele1,ele2=float('-inf'),float('-inf')
+        for i in range(n):
+            if cnt1==0 and nums[i]!=ele2:
+                cnt1=1
+                ele1=nums[i]
+            elif cnt2==0 and nums[i]!=ele1:
+                cnt2=1
+                ele2=nums[i]
+            elif nums[i]==ele1:
+                cnt1+=1
+            elif nums[i]==ele2:
+                cnt2+=1
             else:
-                d[i]+=1
-        for key,value in d.items():
-            if value>len(nums)//3:
-                ans.append(key)
+                cnt1-=1
+                cnt2-=1
+        cnt1,cnt2=0,0
+        ans=[]
+        for i in range(n):
+            if nums[i]==ele1:
+                cnt1+=1
+            elif nums[i]==ele2:
+                cnt2+=1
+        check=int(n/3)+1
+        if cnt1>=check:
+            ans.append(ele1)
+        if cnt2>=check:
+            ans.append(ele2)
         return ans
