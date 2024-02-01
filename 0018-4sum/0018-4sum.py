cecbab5686f8ad7d1bf4b05c0805d2a47ac7257e
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-02-01 11:31:10 +0530

    Time: 435 ms (65.89%), Space: 16.7 MB (60.77%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,17 +1,32 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
         n=len(nums)
-        st=set()
+        ans=[]
         for i in range(n):
+            if i>0 and nums[i]==nums[i-1]:
+                continue
             for j in range(i+1,n):
-                hashmap=set()
-                for k in range(j+1,n):
-                    first_three=nums[i]+nums[j]+nums[k]
-                    left_one=target-first_three
-                    if left_one in hashmap:
-                        temp=[nums[i],nums[j],nums[k],left_one]
-                        temp.sort()
-                        st.add(tuple(temp))
-                    hashmap.add(nums[k])
-        return [list(i) for i in st]
-                            
+                if j>i+1 and nums[j]==nums[j-1]:
+                    continue
+                k=j+1
+                l=n-1
+                while k<l:
+                    sum1=nums[i]+nums[j]+nums[k]+nums[l]
+                    if sum1<target:
+                        k+=1
+                    elif sum1>target:
+                        l-=1
+                    else:
+                        temp=[nums[i],nums[j],nums[k],nums[l]]
+                        ans.append(temp)
+                        k+=1
+                        l-=1
+                        
+                        while k<l and nums[k]==nums[k-1]:
+                            k+=1
+                        
+                        while k<l and nums[l]==nums[l+1]:
+                            l-=1
+        return ans
+                        
