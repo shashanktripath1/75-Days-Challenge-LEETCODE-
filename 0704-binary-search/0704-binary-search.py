a09248a27dddb54efd3b09102ec033664f75038d
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-02-03 16:07:08 +0530

    Time: 196 ms (60.59%), Space: 18.1 MB (65.30%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,15 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n=len(nums)
+        low,high=0,n-1   
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]>target:
+                high=mid-1
+            else:
+                low=mid+1
+        return -1
+                
+            
