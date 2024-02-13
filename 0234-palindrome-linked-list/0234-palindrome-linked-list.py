7082ad0bbfd630614227b4bda45c52611f839ede
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-02-13 13:17:57 +0530

    Time: 331 ms (30.26%), Space: 42 MB (9.11%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -4,18 +4,30 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    def reverse_ll(self,head):
+        if head is None or head.next is None:
+            return head
+        new_head=self.reverse_ll(head.next)
+        front=head.next
+        front.next=head
+        head.next=None
+        return new_head
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        stack=[]
-        temp=head
-        while temp is not None:
-            stack.append(temp.val)
-            temp=temp.next
-        temp=head
-        while temp is not None:
-            if temp.val!=stack.pop():
+        if head is None or head.next is None:
+            return True
+        slow,fast=head,head
+        while fast.next is not None and fast.next.next is not None:
+            slow=slow.next
+            fast=fast.next.next
+        new_head=self.reverse_ll(slow.next)
+        first=head
+        second=new_head
+        while second is not None:
+            if first.val!=second.val:
+                self.reverse_ll(new_head)
                 return False
-            temp=temp.next
+            first=first.next
+            second=second.next
+        self.reverse_ll(new_head)
         return True
-            
-            
-        
+                
