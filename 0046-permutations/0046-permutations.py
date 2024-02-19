82edd49566b7c69879596d939d820f845d914aaa
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-02-19 13:30:27 +0530

    Time: 41 ms (60.51%), Space: 16.6 MB (88.55%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,18 +1,14 @@
 class Solution:
-    def helper(self,nums,freq,ans,ds):
-        if len(ds)==len(nums):
-            ans.append(ds[:])
+    def helper(self,ind,nums,ans):
+        if ind==len(nums):
+            ans.append(nums[:])
             return
-        for i in range(len(nums)):
-            if  not freq[i]:
-                ds.append(nums[i])
-                freq[i]=1
-                self.helper(nums,freq,ans,ds)
-                freq[i]=0
-                ds.pop()
+        for i in range(ind,len(nums)):
+            nums[ind],nums[i]=nums[i],nums[ind]
+            self.helper(ind+1,nums,ans)
+            nums[ind],nums[i]=nums[i],nums[ind]
+            
     def permute(self, nums: List[int]) -> List[List[int]]:
         ans=[]
-        ds=[]
-        freq=[0]*len(nums)
-        self.helper(nums,freq,ans,ds)
+        self.helper(0,nums,ans)
         return ans
