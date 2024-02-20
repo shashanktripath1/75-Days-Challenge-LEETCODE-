76fdcd8900a9c188fd288e7756763c4ae37a8ff9
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-02-20 10:44:32 +0530

    Time: 56 ms (56.94%), Space: 16.8 MB (99.78%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,42 +1,25 @@
 class Solution:
-    def is_safe(self,row,col,board,ans,n):
-        Row=row
-        Col=col
-        while row>=0 and col>=0:
-            if board[row][col]=="Q":
-                return False
-            row-=1
-            col-=1
-        row=Row
-        col=Col
-        
-        while col>=0:
-            if board[row][col]=="Q":
-                return False
-            col-=1
-        row=Row
-        col=Col
-        
-        while row<n and col>=0:
-            if board[row][col]=="Q":
-                return False
-            row+=1
-            col-=1
-        return True
-            
-    def solve(self,col,board,ans,n):
+    def solve(self,col,board,ans,left_row,lower_diagonal,upper_diagonal,n):
         if col==n:
-            ans.append(list(board))
+            ans.append(board.copy())
             return
         for row in range(n):
-            if self.is_safe(row,col,board,ans,n)==True:
+            if left_row[row]==0 and lower_diagonal[row+col]==0 and upper_diagonal[n-1+col-row]==0:
                 board[row]=board[row][:col]+ "Q" +board[row][col+1:]
-                self.solve(col+1,board,ans,n)
-                board[row]=board[row][:col]+ "." +board[row][col+1:]
+                left_row[row]=1
+                lower_diagonal[row+col]=1
+                upper_diagonal[n-1+col-row]=1
+                self.solve(col+1,board,ans,left_row,lower_diagonal,upper_diagonal,n)
                 
-            
+                board[row]=board[row][:col]+ "." +board[row][col+1:]
+                left_row[row]=0
+                lower_diagonal[row+col]=0
+                upper_diagonal[n-1+col-row]=0
     def solveNQueens(self, n: int) -> List[List[str]]:
         ans=[]
-        board=["."*n for _ in range(n)]
-        self.solve(0,board,ans,n)
+        board=["." *(n) for _ in range(n)]
+        left_row=[0]*n
+        lower_diagonal=[0]*(2*n-1)
+        upper_diagonal=[0]*(2*n-1)
+        self.solve(0,board,ans,left_row,lower_diagonal,upper_diagonal,n)
         return ans
