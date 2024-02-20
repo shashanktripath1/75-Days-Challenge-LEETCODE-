0e17ae455f11408d4c0d9261c5789ba2be950f2f
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-02-20 09:56:16 +0530

    Time: 102 ms (19.01%), Space: 16.8 MB (98.55%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,27 +1,42 @@
 class Solution:
-    def helper(self,row,col,pos_dig,neg_dig,ans,board,n):
-        if row==n:
-            store_ans=["".join(r) for r in board]
-            ans.append(store_ans)
-        for c in range(n):
-            if c in col or row+c in pos_dig or row-c in neg_dig:
-                continue
-            col.add(c)
-            pos_dig.add(row+c)
-            neg_dig.add(row-c)
-            board[row][c]='Q'
+    def is_safe(self,row,col,board,ans,n):
+        Row=row
+        Col=col
+        while row>=0 and col>=0:
+            if board[row][col]=="Q":
+                return False
+            row-=1
+            col-=1
+        row=Row
+        col=Col
+        
+        while col>=0:
+            if board[row][col]=="Q":
+                return False
+            col-=1
+        row=Row
+        col=Col
+        
+        while row<n and col>=0:
+            if board[row][col]=="Q":
+                return False
+            row+=1
+            col-=1
+        return True
             
-            self.helper(row+1,col,pos_dig,neg_dig,ans,board,n)
+    def solve(self,col,board,ans,n):
+        if col==n:
+            ans.append(list(board))
+            return
+        for row in range(n):
+            if self.is_safe(row,col,board,ans,n)==True:
+                board[row]=board[row][:col]+ "Q" +board[row][col+1:]
+                self.solve(col+1,board,ans,n)
+                board[row]=board[row][:col]+ "." +board[row][col+1:]
+                
             
-            col.remove(c)
-            pos_dig.remove(row+c)
-            neg_dig.remove(row-c)
-            board[row][c]="."
     def solveNQueens(self, n: int) -> List[List[str]]:
-        col=set()
-        pos_dig=set()
-        neg_dig=set()
         ans=[]
-        board=[["."]*n for _ in range(n )]
-        self.helper(0,col,pos_dig,neg_dig,ans,board,n)
+        board=["."*n for _ in range(n)]
+        self.solve(0,board,ans,n)
         return ans
