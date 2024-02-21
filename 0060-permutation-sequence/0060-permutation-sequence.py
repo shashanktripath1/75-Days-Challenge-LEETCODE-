9b559af1e0dff161eb71bf3e089c08aad6e2716a
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-02-21 09:12:33 +0530

    Time: 43 ms (38.57%), Space: 16.7 MB (70.26%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -0,0 +1,19 @@
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        fact=1
+        numbers=[]
+        for i in range(1,n):
+            fact*=i
+            numbers.append(i)
+        numbers.append(n)
+        ans=""
+        k-=1
+        while(1):
+            ans+=str(numbers[k//fact])
+            numbers.pop(k//fact)
+            if not numbers:
+                break
+            k%=fact
+            fact//=len(numbers)
+        return ans
+            
