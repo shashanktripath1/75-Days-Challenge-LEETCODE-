9f3242574ceb6c3de8e16f58970c5bca8ca77423
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-02-21 11:55:30 +0530

    Time: 39 ms (30.13%), Space: 16.5 MB (76.94%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,11 +1,10 @@
 #MEMOIZATION
 class Solution:
     def f(self,n,dp):
-        if n==0 or n==1:
-            return 1
-        if dp[n]!=-1:
-            return dp[n]
-        dp[n]=self.f(n-1,dp)+self.f(n-2,dp)
+        dp[0]=1
+        dp[1]=1
+        for i in range(2,n+1):
+            dp[i]=dp[i-1]+dp[i-2]
         return dp[n]
     def climbStairs(self, n: int) -> int:
         dp=[-1]*(n+1)
