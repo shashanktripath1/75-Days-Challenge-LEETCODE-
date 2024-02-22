911339903710a3668dc1366d7005d88db2bcf79f
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-02-22 16:31:16 +0530

    Time: 79 ms (84.98%), Space: 17.1 MB (99.40%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,32 +1,23 @@
 class Solution:
-    def f(self,i,j,grid,dp):
-        if i==0 and j==0:
-            return grid[0][0]
-        if i<0 or j<0:
-            return float('inf')
-        if dp[i][j]!=-1:
-            return dp[i][j]
-        up=grid[i][j]+self.f(i-1,j,grid,dp)
-        left=grid[i][j]+self.f(i,j-1,grid,dp)
-        dp[i][j]=min(up,left)
-        return dp[i][j]
     def minPathSum(self, grid: List[List[int]]) -> int:
         m,n=len(grid),len(grid[0])
-        dp=[[-1 for i in range(n+1)]for j in range(m+1)]
+        prev=[0]*n
         for i in range(m):
+            temp=[0]*n
             for j in range(n):
                 if i==0 and j==0:
-                    dp[i][j]=grid[i][j]
+                    temp[j]=grid[i][j]
                 else:
-                    up,left=0,0
+                    up=grid[i][j]
                     if i>0:
-                        up=grid[i][j]+dp[i-1][j]
+                        up+=prev[j]
                     else:
-                        up+=int(1e9)
+                        up=int(1e9)
+                    left=grid[i][j]
                     if j>0:
-                        left=grid[i][j]+dp[i][j-1]
+                        left+=temp[j-1]
                     else:
-                        left+=int(1e9)
-                    dp[i][j]=min(up,left)
-        return dp[m-1][n-1]
-                
+                        left=int(1e9)
+                    temp[j]=min(up,left)
+            prev=temp
+        return prev[n-1]
