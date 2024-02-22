36ac0c002317d9ca58ab12c09f4e61e4be3ed9d5
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-02-22 15:41:49 +0530

    Time: 43 ms (69.55%), Space: 16.7 MB (58.89%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,36 +1,25 @@
 class Solution:
-    def f(self,i,j,mat,dp):
-        if i>=0 and j>=0 and mat[i][j]== 1:
-            return 0
-        if i==0 and j==0:
-            return 1
-        if i<0 or j<0:
-            return 0
-        if dp[i][j]!=-1:
-            return dp[i][j]
-        up=self.f(i-1,j,mat,dp)
-        left=self.f(i,j-1,mat,dp)
-        dp[i][j]=up+left
-        return dp[i][j]
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        m=len(obstacleGrid)
-        n=len(obstacleGrid[0])
-        if obstacleGrid[0][0]==1 or obstacleGrid[m-1][n-1]==1:
+        m,n=len(obstacleGrid),len(obstacleGrid[0])
+        if obstacleGrid[0][0] or obstacleGrid[m-1][n-1]:
             return 0
-        dp=[[-1 for i in range(n+1)]for j in range(m+1)]
+        prev=[0]*n
         for i in range(m):
+            temp=[0]*n
             for j in range(n):
+                if obstacleGrid[i][j]==1:
+                    prev[j]=0
+                    continue
                 if i==0 and j==0:
-                    dp[i][j]=1
+                    temp[j]=1
                     continue
-                if  obstacleGrid[i][j]==1:
-                    dp[i][j]=0
-                    continue
-            
                 up,left=0,0
                 if i>0:
-                    up=dp[i-1][j]
+                    up=prev[j]
                 if j>0:
-                    left=dp[i][j-1]
-                dp[i][j]=up+left
-        return dp[m-1][n-1]
+                    left=temp[j-1]
+                temp[j]=up+left
+            prev=temp
+        return prev[n-1]
+                
+                
