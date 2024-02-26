c32fbd464ec43804eb53062e65e3b759bc616c3e
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-02-26 11:37:58 +0530

    Time: 275 ms (42.76%), Space: 37.7 MB (34.45%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -14,5 +14,15 @@ class Solution:
         return dp[ind][target]
     def change(self, amount: int, coins: List[int]) -> int:
         n=len(coins)
-        dp=[[-1 for j in range(amount+1)]for i in range(n)]
-        return self.f(n-1,amount,coins,dp)
+        dp=[[0 for j in range(amount+1)]for i in range(n+1)]
+        for j in range(amount+1):
+            if j%coins[0]==0:
+                dp[0][j]=1
+        for ind in range(1,n):
+            for target in range(amount+1):
+                not_take=0+dp[ind-1][target]
+                take=0
+                if coins[ind]<=target:
+                    take=dp[ind][target-coins[ind]]
+                dp[ind][target]=not_take+take
+        return dp[n-1][amount]
