03500543fcc148e2902cfd3592018461c37e7815
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-02-27 12:21:51 +0530

    Time: 862 ms (5.25%), Space: 146.8 MB (33.74%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,27 +1,14 @@
 class Solution:
-    def f(self,i,j,s,t,dp):
+    @cache
+    def f(self,i,j,s,t):
         if j<0:
             return 1
         if i<0:
             return 0
-        if dp[i][j]!=-1:
-            return dp[i][j]
         if s[i]==t[j]:
-            dp[i][j]=self.f(i-1,j-1,s,t,dp)+self.f(i-1,j,s,t,dp)
+            return self.f(i-1,j-1,s,t)+self.f(i-1,j,s,t)
         
-        else:
-            dp[i][j]=self.f(i-1,j,s,t,dp)
-        return dp[i][j]
-    #single array space optimization
+        return self.f(i-1,j,s,t)
     def numDistinct(self, s: str, t: str) -> int:
         m,n=len(s),len(t)
-        prev=[0]*(n+1)
-        prev[0]=1
-        for i in range(1,m+1):
-            for j in range(n,0,-1):
-                if s[i-1]==t[j-1]:
-                    prev[j]=prev[j-1]+prev[j]
-                else:
-                    prev[j]=prev[j]
-        return prev[n]
-        
+        return self.f(m-1,n-1,s,t)
