25ce1a81647bb5ef6ac6d12a42f65d34db61a166
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-02-27 10:36:57 +0530

    Time: 463 ms (61.04%), Space: 42.5 MB (69.93%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.py
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.py
@@ -1,18 +1,4 @@
 class Solution:
-    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        m,n=len(text1),len(text2)
-        dp=[[-1 for j in range(n+1)]for i in range(m+1)]
-        for i in range(m+1):
-            dp[i][0]=0
-        for j in range(n+1):
-            dp[0][j]=0
-        for ind1 in range(1,m+1):
-            for ind2 in range(1,n+1):
-                if text1[ind1-1]==text2[ind2-1]:
-                    dp[ind1][ind2]=1+dp[ind1-1][ind2-1]
-                else:
-                    dp[ind1][ind2]=0+max(dp[ind1-1][ind2],dp[ind1][ind2-1])
-        return dp[m][n]
                 
     def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
         m,n=len(str1),len(str2)
