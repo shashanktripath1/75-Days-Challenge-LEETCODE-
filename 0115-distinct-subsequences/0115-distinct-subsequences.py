f57d073d0150cfc6b095b53edb247f73176f904b
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-02-27 12:12:47 +0530

    Time: 315 ms (54.43%), Space: 73.8 MB (44.23%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -14,5 +14,16 @@ class Solution:
         return dp[i][j]
     def numDistinct(self, s: str, t: str) -> int:
         m,n=len(s),len(t)
-        dp=[[-1 for j in range(n+1)]for i in range(m+1)]
-        return self.f(m-1,n-1,s,t,dp)
+        dp=[[0 for j in range(n+1)]for i in range(m+1)]
+        for i in range(m+1):
+            dp[i][0]=1
+        for j in range(1,n+1):#trick as the zeroth one is already filled in previous loop so starting from 1 
+            dp[0][j]=0
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if s[i-1]==t[j-1]:
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j]
+                else:
+                    dp[i][j]=dp[i-1][j]
+        return dp[m][n]
+        
