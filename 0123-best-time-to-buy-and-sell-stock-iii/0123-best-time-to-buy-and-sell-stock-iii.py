5a45a04125b6d17e9bd3b7fa5f5491c34fe3ed5f
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-02-29 12:36:26 +0530

    Time: 1152 ms (30.40%), Space: 30.6 MB (73.45%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -14,15 +14,17 @@ class Solution:
         return dp[ind][buy][cap]
     def maxProfit(self, prices: List[int]) -> int:
         n=len(prices)
-        dp=[[[0 for _ in range(3)]for _ in range(2)]for _ in range(n+1)]
+        ahead = [[0 for _ in range(3)] for _ in range(2)]
+        cur = [[0 for _ in range(3)] for _ in range(2)]        
         for ind in range(n-1,-1,-1):
             for buy in range(2):
                 for cap in range(1,3):#point to remember wnen do revision
                     if buy==0:
-                        op1=0+dp[ind+1][0][cap]
-                        op2=-prices[ind]+dp[ind+1][1][cap]
+                        op1=0+ahead[0][cap]
+                        op2=-prices[ind]+ahead[1][cap]
                     else:
-                        op1=0+dp[ind+1][1][cap]
-                        op2=prices[ind]+dp[ind+1][0][cap-1]
-                    dp[ind][buy][cap]=max(op1,op2)
-        return dp[0][0][2]
+                        op1=0+ahead[1][cap]
+                        op2=prices[ind]+ahead[0][cap-1]
+                    cur[buy][cap]=max(op1,op2)
+                ahead=cur
+        return ahead[0][2]
