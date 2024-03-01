b4aab3d0e0e9f2d4c8a959e164a8082ca81f3589
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-03-01 16:46:26 +0530

    Time: 1763 ms (56.29%), Space: 16.9 MB (81.60%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -2,15 +2,11 @@ class Solution:
     #space optimized using next and cur array
     def lengthOfLIS(self, nums: List[int]) -> int:
         n=len(nums)
-        next=[0]*(n+1)
-        cur=[0]*(n+1)
-        for ind in range(n-1,-1,-1):
-            for prev_ind in range(ind-1,-2,-1):
-                not_take=0+next[prev_ind+1]#due to coordinate shift
-                take=0
-                if prev_ind==-1 or nums[ind]>nums[prev_ind]:
-                    take=1+next[ind+1]
-                cur[prev_ind+1]=max(not_take,take)
-            next=cur
-        return cur[-1+1]
-    
+        dp=[1]*(n)
+        ans=1
+        for ind in range(n):
+            for prev_ind in range(ind):
+                if nums[ind]>nums[prev_ind]:
+                    dp[ind]=max(dp[ind],1+dp[prev_ind])
+            ans=max(ans,dp[ind])
+        return ans
