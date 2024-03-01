52249d48c6e00f627cc407d659c102b36252558e
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-03-01 16:28:10 +0530

    Time: 4037 ms (5.00%), Space: 16.9 MB (95.64%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,25 +1,16 @@
 class Solution:
-    def f(self,ind,prev_ind,arr,dp):
-        if ind==len(arr):
-            return 0
-        if dp[ind][prev_ind]!=-1:
-            return dp[ind][prev_ind]
-        not_take=0+self.f(ind+1,prev_ind,arr,dp)
-        take=float('-inf')
-        if prev_ind==-1 or arr[ind]>arr[prev_ind]:
-            take=1+self.f(ind+1,ind,arr,dp)
-        dp[ind][prev_ind]=max(not_take,take)
-        return dp[ind][prev_ind]
-    #tabulation    
+    #space optimized using next and cur array
     def lengthOfLIS(self, nums: List[int]) -> int:
         n=len(nums)
-        dp=[[0 for i in range(n+1)]for j in range(n+1)]
+        next=[0]*(n+1)
+        cur=[0]*(n+1)
         for ind in range(n-1,-1,-1):
             for prev_ind in range(ind-1,-2,-1):
-                not_take=0+dp[ind+1][prev_ind+1]#due to coordinate shift
+                not_take=0+next[prev_ind+1]#due to coordinate shift
                 take=0
                 if prev_ind==-1 or nums[ind]>nums[prev_ind]:
-                    take=1+dp[ind+1][ind+1]
-                dp[ind][prev_ind+1]=max(not_take,take)
-        return dp[0][-1+1]
+                    take=1+next[ind+1]
+                cur[prev_ind+1]=max(not_take,take)
+            next=cur
+        return cur[-1+1]
     
