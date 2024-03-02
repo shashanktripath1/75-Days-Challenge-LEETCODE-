f839a89ad2e9f15769508a315ec4c02924c38fd5
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-03-02 22:40:59 +0530

    Time: 512 ms (84.37%), Space: 16.8 MB (98.36%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
@@ -1,20 +1,19 @@
 class Solution:
-    def f(self,i,j,cuts,dp):
-        if i>j:
-            return 0
-        if dp[i][j]!=-1:
-            return dp[i][j]
-        mini=float('inf')
-        for ind in range(i,j+1):
-            cost=cuts[j+1]-cuts[i-1]+self.f(i,ind-1,cuts,dp)+self.f(ind+1,j,cuts,dp)
-            mini=min(mini,cost)
-        dp[i][j]=mini
-        return dp[i][j]
     def minCost(self, n: int, cuts: List[int]) -> int:
+        c=len(cuts)
         cuts.append(n)
         cuts.insert(0,0)
         cuts.sort()
-        c=len(cuts)
-        dp=[[-1 for _ in range(c+1)]for _ in range(c+1)]
-        return self.f(1,c-2,cuts,dp)
+        dp=[[0 for _ in range(c+2)]for _ in range(c+2)]
+        for i in range(c,0,-1):
+            for j in range(1,c+1):
+                if i>j:
+                    continue
+                mini=float('inf')
+                for ind in range(i,j+1):
+                    cost=cuts[j+1]-cuts[i-1]+dp[i][ind-1]+dp[ind+1][j]
+                    mini=min(mini,cost)
+                dp[i][j]=mini
+        return dp[1][c]
+                
     
