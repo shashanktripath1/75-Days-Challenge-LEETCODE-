5855bf1f72e11af826fd3a7f9c4db9dfd20360cb
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-03-03 00:28:46 +0530

    Time: 2752 ms (81.87%), Space: 18.6 MB (79.41%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.py
+++ 0312-burst-balloons/0312-burst-balloons.py
@@ -15,5 +15,14 @@ class Solution:
         n=len(nums)
         nums.append(1)
         nums.insert(0,1)
-        dp=[[-1 for _ in range(n+1)]for _ in range(n+1)]
-        return self.f(1,n,nums,dp)
+        dp=[[0 for _ in range(n+2)]for _ in range(n+2)]
+        for i in range(n,0,-1):
+            for j in range(1,n+1):
+                if i>j:
+                    continue
+                maxi=float('-inf')
+                for ind in range(i,j+1):
+                    cost=nums[i-1] * nums[ind] * nums[j+1] +dp[i][ind-1]+dp[ind+1][j]
+                    maxi=max(maxi,cost)
+                dp[i][j]=maxi
+        return dp[1][n]
