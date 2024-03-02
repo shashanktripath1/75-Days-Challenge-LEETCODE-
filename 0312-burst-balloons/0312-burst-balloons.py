4a2c7b1dae5477a3dadb3c06b7869e419314fcd8
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-03-03 00:29:00 +0530

    Time: 2764 ms (81.15%), Space: 18.5 MB (96.52%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.py
+++ 0312-burst-balloons/0312-burst-balloons.py
@@ -1,16 +1,4 @@
 class Solution:
-    
-    def f(self,i,j,nums,dp):
-        if i>j:
-            return 0
-        if dp[i][j]!=-1:
-            return dp[i][j]
-        maxi=float('-inf')
-        for ind in range(i,j+1):
-            cost=nums[i-1] * nums[ind] * nums[j+1] +self.f(i,ind-1,nums,dp)+self.f(ind+1,j,nums,dp)
-            maxi=max(maxi,cost)
-        dp[i][j]=maxi
-        return dp[i][j]
     def maxCoins(self, nums: List[int]) -> int:
         n=len(nums)
         nums.append(1)
