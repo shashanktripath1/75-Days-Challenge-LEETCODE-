9c33fb2a61629e6bdf9f2350bc64642c45972cdb
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-03-03 15:21:30 +0530

    Time: 358 ms (35.46%), Space: 16.7 MB (69.71%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
@@ -1,16 +1,20 @@
-#tabulation
 class Solution:
+    def f(self,ind,k,arr,n,dp):
+        if ind==n:
+            return 0
+        if dp[ind]!=-1:
+            return dp[ind]
+        len1=0
+        max_ans=float('-inf')
+        maxi=float('-inf')
+        for j in range(ind,min(ind+k,n)):
+            len1+=1
+            maxi=max(maxi,arr[j])
+            summation=maxi*len1 +self.f(j+1,k,arr,n,dp)
+            max_ans=max(max_ans,summation)
+        dp[ind]=max_ans
+        return dp[ind]
     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
         n=len(arr)
-        dp=[0]*(n+1)
-        for ind in range(n-1,-1,-1):
-            len1=0
-            max_ans=float('-inf')
-            maxi=float('-inf')
-            for j in range(ind,min(n,ind+k)):
-                len1+=1
-                maxi=max(maxi,arr[j])
-                sum1=(len1 * maxi)+dp[j+1]
-                max_ans=max(max_ans,sum1)
-            dp[ind]=max_ans
-        return dp[0]
+        dp=[-1]*(n)
+        return self.f(0,k,arr,n,dp)
