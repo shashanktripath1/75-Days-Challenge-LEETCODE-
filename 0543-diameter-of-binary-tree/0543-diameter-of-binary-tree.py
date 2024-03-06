29bf0c02a705272565dfb117b09bd4c28319f7f5
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-03-06 10:44:19 +0530

    Time: 51 ms (12.97%), Space: 19.2 MB (8.51%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -7,6 +7,7 @@
 '''The issue with your code lies in the way you are updating the diameter variable inside the dfs function. In Python, integers are immutable, and when you pass diameter to the recursive calls, you are essentially passing its value, not a reference. Therefore, any modifications inside the recursive calls won't affect the original diameter in the outer scope.
 
 To fix this, you can use a mutable object like a list to hold the diameter value and pass it through the recursive calls. Here's the corrected code:'''
+# else we can use this nonlocal concept
 class Solution:
     
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
