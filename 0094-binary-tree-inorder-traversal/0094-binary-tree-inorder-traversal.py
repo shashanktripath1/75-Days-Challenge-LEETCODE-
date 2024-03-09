94bcaf33348d72512ee7c968f51e58324d03c7c0
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-03-09 14:49:38 +0530

    Time: 38 ms (37.43%), Space: 16.6 MB (40.02%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -5,16 +5,37 @@
 #         self.left = left
 #         self.right = right
 # left root right
+#Morris traversal
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        ans,stack=[],[]
-        cur=root
-        while stack or cur:
-            if cur:
-                stack.append(cur)
-                cur=cur.left
-            else:
-                cur=stack.pop()
+        ans = []
+        cur = root
+        while cur != None:
+            #printing the leftmost node
+            if not cur.left:
                 ans.append(cur.val)
-                cur=cur.right
+                cur = cur.right
+            else:
+                temp = cur
+                temp = temp.left
+                #going to the rightmost node in the left subtree (lets call it temp)
+                while temp.right and temp.right != cur:
+                    temp = temp.right
+                
+                #2 conditions arise:
+                
+                #i. the right child of temp doesn't exist (The thread to the cur node has not been made)
+                #in this case, point the right child of temp to cur and move cur to its left child
+                if not temp.right:
+                    temp.right = cur
+                    cur = cur.left
+
+                #ii. the thread has already been created so we break the thread
+                #(pointing the temp's right child back to None)and print cur.
+                #Finally, move cur to its right child      
+                else:
+                    ans.append(cur.val)
+                    temp.right = None
+                    cur = cur.right
+
         return ans
