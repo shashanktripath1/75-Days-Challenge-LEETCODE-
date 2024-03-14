847a1682ecc572cdcf717d3aad2ab2615d19983d
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-03-14 14:36:02 +0530

    Time: 490 ms (54.85%), Space: 18.1 MB (98.17%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -1,26 +1,30 @@
+from collections import deque
 class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
-        #this bfs function will make all the boundary 1's and the 1's connected to them as 0
-        def dfs(i,j):
-            if i<0 or i>=m or j<0 or j>=n or grid[i][j]==0:
-                return 0
-            grid[i][j]=0
-            dfs(i+1,j)
-            dfs(i-1,j)
-            dfs(i,j-1)
-            dfs(i,j+1)
-        
-        m,n=len(grid),len(grid[0])
-        for i in range(m):
-            for j in range(n):
-                if (i==0 or i==m-1 or j==0 or j==n-1) and grid[i][j]==1:
-                    dfs(i,j)
+        queue=deque()
         count=0
-        #now counting the remaining 1's and that will be our answer
+        m=len(grid)
+        n=len(grid[0])
+        visited=[[0 for _ in range(n)]for _ in range(m)]
         for i in range(m):
             for j in range(n):
-                if grid[i][j]==1:
+                if i==0 or j ==0 or i==m-1 or j==n-1 :
+                    if grid[i][j]==1:
+                        queue.append((i,j))
+                        visited[i][j]=1
+        deltarow=[1,-1,0,0]
+        deltacol=[0,0,1,-1]
+        while queue:
+            row,col=queue.popleft()
+            for i in range(4):
+                nr=row+deltarow[i]
+                nc=col+deltacol[i]
+                if 0<=nr<m and 0<=nc<n and visited[nr][nc]==0 and grid[nr][nc]==1:
+                    queue.append((nr,nc))
+                    visited[nr][nc]=1
+        for i in range(m):
+            for j in range(n):
+                if visited[i][j]==0 and grid[i][j]==1:
                     count+=1
         return count
                     
-
