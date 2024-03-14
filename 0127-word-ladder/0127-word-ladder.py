e74e994918452f7fbadf1e5e33e3b114482cdf2a
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-03-14 21:12:08 +0530

    Time: 368 ms (42.24%), Space: 17.5 MB (98.16%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -24,7 +24,7 @@ class Solution:
                     new_word = word[:i] + ch + word[i+1:]
                     # Check if it exists in the set and push it into the queue.
                     if new_word in word_set:
-                        word_set.remove(new_word)
+                        word_set.discard(new_word)
                         q.append((new_word, steps + 1))
 
         # If there is no transformation sequence possible
