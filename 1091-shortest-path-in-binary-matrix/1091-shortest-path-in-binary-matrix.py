ad668831fa5d2cf4d5d5e86640a19da45cf36d58
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-03-18 23:58:05 +0530

    Time: 447 ms (53.82%), Space: 18.3 MB (10.04%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,45 +1,24 @@
+from collections import deque
+
 class Solution:
-    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        # check if source and target are not clear cells
-        if grid[0][0] != 0 or grid[-1][-1] != 0:
+    def shortestPathBinaryMatrix(self, grid):
+        n = len(grid)
+        if grid[0][0] == 1 or grid[n - 1][n - 1] == 1:
             return -1
         
-        N = len(grid)            
-        # offsets required for all 8 directions
-        offsets = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (-1, 1), (1, -1), (-1, -1)]
+        queue = deque([(0, 0, 1)])  # (row, col, path_length)
+        visited = set([(0, 0)])
         
-        q = deque()
-        q.append((0,0)) # starting point
-        visited = {(0, 0)}
-        
-        
-        # finds unvisited clear cells using 8 offsets
-        def get_neighbours(x,y):
-            for x_offset, y_offset in offsets:
-                new_row = x + x_offset
-                new_col = y + y_offset
-                
-                if 0 <= new_row < N and 0 <= new_col < N and not grid[new_row][new_col] and (new_row, new_col) not in visited:
-                    yield (new_row, new_col)                                                
+        while queue:
+            row, col, path_length = queue.popleft()
+            if (row, col) == (n - 1, n - 1):
+                return path_length
             
+            for dr, dc in directions:
+                new_row, new_col = row + dr, col + dc
+                if 0 <= new_row < n and 0 <= new_col < n and grid[new_row][new_col] == 0 and (new_row, new_col) not in visited:
+                    visited.add((new_row, new_col))
+                    queue.append((new_row, new_col, path_length + 1))
         
-        current_distance = 1 # start with one clear cell
-        # standard iterative BFS traversal
-        while q:
-            length = len(q)
-            
-            # loop through all the cells at the same distance
-            for _ in range(length):
-                row, col = q.popleft()
-                
-                if row == N-1 and col==N-1: # reached target
-                    return current_distance
-                
-                # loop though all valid neignbours
-                for p in get_neighbours(row, col):
-                    visited.add(p)
-                    q.append(p)
-                                    
-            current_distance+=1 # update the level or distance from source
-        
-        return -1    
+        return -1
