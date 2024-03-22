c605d3882b975bc1c85b0bc82e9927ee476d6cbd
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-03-22 07:33:08 +0530

    Time: 393 ms (47.07%), Space: 36.8 MB (52.57%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -0,0 +1,56 @@
+class DisjointSet:
+    def __init__(self, n):
+        self.rank = [0] * (n + 1)
+        self.parent = [i for i in range(n + 1)]
+        self.size = [1] * (n + 1)
+
+    def findUPar(self, node):
+        if node == self.parent[node]:
+            return node
+        self.parent[node] = self.findUPar(self.parent[node])
+        return self.parent[node]
+
+    def unionByRank(self, u, v):
+        ulp_u = self.findUPar(u)
+        ulp_v = self.findUPar(v)
+        if ulp_u == ulp_v:
+            return
+        if self.rank[ulp_u] < self.rank[ulp_v]:
+            self.parent[ulp_u] = ulp_v
+        elif self.rank[ulp_v] < self.rank[ulp_u]:
+            self.parent[ulp_v] = ulp_u
+        else:
+            self.parent[ulp_v] = ulp_u
+            self.rank[ulp_u] += 1
+
+    def unionBySize(self, u, v):
+        ulp_u = self.findUPar(u)
+        ulp_v = self.findUPar(v)
+        if ulp_u == ulp_v:
+            return
+        if self.size[ulp_u] < self.size[ulp_v]:
+            self.parent[ulp_u] = ulp_v
+            self.size[ulp_v] += self.size[ulp_u]
+        else:
+            self.parent[ulp_v] = ulp_u
+            self.size[ulp_u] += self.size[ulp_v]
+
+class Solution:
+    def makeConnected(self, n, connections):
+        ds = DisjointSet(n)
+        cntExtras = 0
+        for u, v in connections:
+            if ds.findUPar(u) == ds.findUPar(v):
+                cntExtras += 1
+            else:
+                ds.unionBySize(u, v)
+        cntC = sum(1 for i in range(n) if ds.parent[i] == i)
+        ans = cntC - 1
+        if cntExtras >= ans:
+            return ans
+        return -1
+
+
+
+
+        
