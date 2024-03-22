e61dc8a3bffa3feddf154a5e34015393ffa43f92
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-03-22 08:32:05 +0530

    Time: 124 ms (81.59%), Space: 17.7 MB (41.21%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
@@ -1,26 +1,67 @@
+class DisjointSet:
+    def __init__(self, n):
+        self.rank = [0] * (n + 1)
+        self.parent = [i for i in range(n + 1)]
+        self.size = [1] * (n + 1)
+
+    def findUPar(self, node):
+        if node == self.parent[node]:
+            return node
+        self.parent[node] = self.findUPar(self.parent[node])
+        return self.parent[node]
+
+    def unionByRank(self, u, v):
+        ulp_u = self.findUPar(u)
+        ulp_v = self.findUPar(v)
+        if ulp_u == ulp_v:
+            return
+        if self.rank[ulp_u] < self.rank[ulp_v]:
+            self.parent[ulp_u] = ulp_v
+        elif self.rank[ulp_v] < self.rank[ulp_u]:
+            self.parent[ulp_v] = ulp_u
+        else:
+            self.parent[ulp_v] = ulp_u
+            self.rank[ulp_u] += 1
+
+    def unionBySize(self, u, v):
+        ulp_u = self.findUPar(u)
+        ulp_v = self.findUPar(v)
+        if ulp_u == ulp_v:
+            return
+        if self.size[ulp_u] < self.size[ulp_v]:
+            self.parent[ulp_u] = ulp_v
+            self.size[ulp_v] += self.size[ulp_u]
+        else:
+            self.parent[ulp_v] = ulp_u
+            self.size[ulp_u] += self.size[ulp_v]
+
 class Solution:
-    def removeStones(self, stones: List[List[int]]) -> int:
+    def removeStones(self, stones):
+        n=len(stones)
+        maxRow = 0
+        maxCol = 0
+        for it in stones:
+            if it[0] > maxRow:
+                maxRow = it[0]
+            if it[1] > maxCol:
+                maxCol = it[1]
         
-        dx = defaultdict(set)
-        dy = defaultdict(set)
+        ds = DisjointSet(maxRow + maxCol + 1)
+        stoneNodes = {}
         
-        for x,y in stones:
-            dx[x].add((x, y))
-            dy[y].add((x, y))
-            
-        visited = set()
+        for it in stones:
+            nodeRow = it[0]
+            nodeCol = it[1] + maxRow + 1
+            ds.unionBySize(nodeRow, nodeCol)
+            stoneNodes[nodeRow] = 1
+            stoneNodes[nodeCol] = 1
+
+        cnt = 0
+        for it in stoneNodes:
+            if ds.findUPar(it) == it:
+                cnt += 1
         
-        def dfs(x, y):
-            visited.add((x, y))
-            child = (dx[x].union(dy[y]) - visited)
-            for p, q in child:
-                dfs(p, q)
-        
-        cc = 0
-        for x,y in stones:
-            if(not (x,y) in visited):
-                dfs(x, y)
-                cc+=1
-        
-        return len(stones)-cc
+        return n - cnt
+
+
         
