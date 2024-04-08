9c78ed0b0355a7d9eae324667a9074a0a04a20d7
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-04-08 10:51:09 +0530

    Time: 41 ms (27.78%), Space: 16.7 MB (31.15%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -2,7 +2,7 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         n=len(nums)
         ans=[]
-        for i in range(2**n):
+        for i in range(1<<n):
             temp=[]
             for j in range(n):
                 if i & (1<<j):
