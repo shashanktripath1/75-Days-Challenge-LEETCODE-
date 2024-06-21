fde0d232bc4d630606b597c9cb5a8427151c78f4
Shashank Tripathi <94044889+shashanktripath1@users.noreply.github.com>
2024-06-21 14:23:50 +0530

    Time: 101 ms (66.63%), Space: 17.4 MB (90.77%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,25 +1,25 @@
+#BRUTE FORCE
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         m,n=len(matrix),len(matrix[0])
-        col0=1
         for i in range(m):
             for j in range(n):
                 if matrix[i][j]==0:
-                    matrix[i][0]=0
-                    if j!=0:
-                        matrix[0][j]=0
-                    else:
-                        col0=0
-        for i in range(1,m):
-            for j in range(1,n):
-                if matrix[i][j]!=0:
-                    if matrix[i][0]==0 or matrix[0][j]==0:
-                        matrix[i][j]=0
-        if matrix[0][0]==0:
+                    self.markRow(matrix,i,m,n)
+                    self.markCol(matrix,j,m,n)
+        for i in range(m):
             for j in range(n):
-                matrix[0][j]=0
-        if col0==0:
-            for i in range(m):
-                matrix[i][0]=0
+                if matrix[i][j]==-123455:
+                    matrix[i][j]=0
         return matrix
-                
+    def markRow(self,matrix,i,m,n):
+        for j in range(n):
+            if matrix[i][j]!=0:
+                matrix[i][j]=-123455
+    def markCol(self,matrix,j,m,n):
+        for i in range(m):
+            if matrix[i][j]!=0:
+                matrix[i][j]=-123455
+    
+            
+        
